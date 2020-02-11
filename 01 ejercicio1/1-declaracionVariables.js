dd397288569b231bcb0a6212eceb0cd45a93aed9
505 <505@academicos.cesde.edu.co>
2020-02-10 20:15:53 -0500

    segundo comit

---
+++ 01 ejercicio1/1-declaracionVariables.js
@@ -0,0 +1,21 @@
+/*let nombre = "swaider";
+let nombre = "andres";
+console.log(nombre);*/
+
+let costo = 5;
+if(costo > 3)
+{
+    var comida = "galleta";
+    if(costo > 4)
+    {
+        let paseo = "jardin";
+    }
+}
+console.log("la comida:" + comida);
+
+for(let i=0; i<5; i++)
+{
+
+}
+console.log(i);
+
